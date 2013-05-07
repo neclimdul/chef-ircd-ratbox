d372a88cce75a3b0ec30f5f5a26910eb4f4f1905
jgilliland <jgilliland@apqc.org>
2013-05-07 15:09:48 -0500

    Split ircd recipe out
    This actuall fixes a bug where default attributes where not detected because
    of the name of the attributes file. It gives more clear control as well.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "rafael+github@bitsignal.com"
 license          "Apache 2.0"
 description      "Installs/Configures ircd-ratbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+version          "0.0.4"
 supports 'centos'
 supports 'fedora'
 supports 'rhel'
